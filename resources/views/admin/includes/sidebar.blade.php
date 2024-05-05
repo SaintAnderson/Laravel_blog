0d48ac14a1fc00f2046fc353212f4fc39ba8d59b
@andrei_pastukhov <pvand.work@gmail.com>
2024-05-06 00:45:07 +0300

    version 2.0

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,19 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>
+                            Категории
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
